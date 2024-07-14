e6609a550bc8a8edf18029e34ea2a649545209db
saadelhamidi <saadavicii123@gmail.com>
2024-07-14 08:45:06 -0700

    x

--- 100-clean_web_static.py
+++ 100-clean_web_static.py
@@ -1,72 +1,80 @@
 #!/usr/bin/python3
-"""Fabric script to distribute an archive to web servers."""
+"""
+Fabric script that distributes an archive to your web servers
+"""
 
 from datetime import datetime
 from fabric.api import *
 import subprocess
 import os
-
-# Define the target hosts and user credentials
 env.hosts = ["34.229.66.77", "18.209.225.222"]
 env.user = "ubuntu"
 env.password = "betty"
 
+
 def do_pack():
-    """Create a .tgz archive from the web_static folder."""
+    """Generate a .tgz archive from the contents of the web_static folder."""
     current_directory = os.getcwd()
-    versions_directory = os.path.join(current_directory, "versions")
-    
-    # Create versions directory if it doesn't exist
-    if not os.path.exists(versions_directory):
+    directory_path = os.path.join(current_directory, "versions")
+    if not os.path.exists(directory_path):
         local("mkdir -p versions")
-    
-    # Generate archive name with timestamp
     timestamp = datetime.utcnow().strftime('%Y%m%d%H%M%S')
-    archive_name = f"web_static_{timestamp}.tgz"
+    archive_name = "web_static_{}.tgz".format(timestamp)
+    print(timestamp)
     archive_path = os.path.join("versions", archive_name)
-    
-    # Create the archive
-    result = local(f"tar -cvzf {archive_path} web_static/")
-    return archive_path if result.ok else None
+    exit_status = local("tar -cvzf {} web_static/".format(archive_path))
+    if exit_status.ok:
+        return archive_path
+    else:
+        return None
+
 
 def do_deploy(archive_path):
-    """Deploy the archive to the web servers."""
+    """
+        Distribute archive.
+    """
     if os.path.exists(archive_path):
-        file_name = os.path.basename(archive_path)
-        base_name = os.path.splitext(file_name)[0]
-        release_dir = f"/data/web_static/releases/{base_name}"
-        tmp_path = f"/tmp/{file_name}"
-        
-        # Transfer and extract the archive
-        put(archive_path, tmp_path)
-        run(f"sudo mkdir -p {release_dir}")
-        run(f"sudo tar -xzf {tmp_path} -C {release_dir}")
-        run(f"sudo rm {tmp_path}")
-        run(f"sudo mv {release_dir}/web_static/* {release_dir}")
-        run(f"sudo rm -rf {release_dir}/web_static")
-        
-        # Update the current symlink
+        name = str(archive_path).split('/')[-1]
+        basename = str(name).split('.')[0]
+        ver_to_deploy = "/data/web_static/releases/" + basename
+        tmpname = "/tmp/" + name
+        put(archive_path, "/tmp/")
+        run("sudo mkdir -p {}".format(ver_to_deploy))
+        run("sudo tar -xzf {} -C {}/".format(tmpname,
+                                             ver_to_deploy))
+        run("sudo rm {}".format(tmpname))
+        run("sudo mv {}/web_static/* {}".format(ver_to_deploy,
+                                                ver_to_deploy))
+        run("sudo rm -rf {}/web_static/".format(ver_to_deploy))
         run("sudo rm -rf /data/web_static/current")
-        run(f"sudo ln -s {release_dir} /data/web_static/current")
-        
+        run("sudo ln -s {}/ /data/web_static/current".format(ver_to_deploy))
         print("New version deployed!")
         return True
-    
+
     return False
+
 
 def deploy():
-    """Execute the full deployment process."""
+    """Full deployment
+    """
     archive_path = do_pack()
-    if archive_path:
-        return do_deploy(archive_path)
-    return False
+    res = do_deploy(archive_path)
+    return (res)
+
 
 def do_clean(number=0):
-    """Remove outdated archives."""
-    number = max(1, int(number))
-    
-    # Clean local archives
-    local(f"ls -1t versions/ | grep '^web_static_' | tail -n +{number + 1} | xargs -I {{}} rm -rf versions/{{}}")
-    
-    # Clean remote archives
-    run(f"ls -1t /data/web_static/releases/ | grep '^web_static_' | tail -n +{number + 1} | xargs -I {{}} sudo rm -rf /data/web_static/releases/{{}}")
+    """  Keep it clean!
+    deletes out-of-date archives,
+    """
+
+    if int(number) < 0:
+        return False
+    num = 1 if int(number) == 0 else int(number)
+    try:
+        local("ls -1t versions/ | grep '^web_static_' | tail -n +{} | \
+              xargs -I {{}} rm -rf versions/{{}}".format(num + 1))
+        run("ls -1t /data/web_static/releases/ | grep '^web_static_' | \
+            tail -n +{} | xargs -I {{}} sudo rm -rf \
+                /data/web_static/releases/{{}}".format(num + 1))
+    except Exception as e:
+        pass
