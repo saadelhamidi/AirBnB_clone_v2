e05a8f8b1b22a929e08cfe025257ca80b5637170
saadelhamidi <saadavicii123@gmail.com>
2024-07-14 02:02:16 -0700

    x

--- 3-deploy_web_static.py
+++ 3-deploy_web_static.py
@@ -1,11 +1,24 @@
 #!/usr/bin/python3
 """
-Fabric script that distributes an archive to your web servers
+Fabric script that creates and distributes an archive to your web servers
 """
-from fabric.api import env, put, run
+from fabric.api import env, put, run, local
+from datetime import datetime
 import os
 
 env.hosts = ['<IP web-01>', '<IP web-02>']  # Replace with your web server IPs
+
+def do_pack():
+    """Generates a .tgz archive from the contents of the web_static folder"""
+    try:
+        if not os.path.exists("versions"):
+            os.mkdir("versions")
+        now = datetime.now()
+        archive_name = "versions/web_static_{}.tgz".format(now.strftime("%Y%m%d%H%M%S"))
+        local("tar -cvzf {} web_static".format(archive_name))
+        return archive_name
+    except:
+        return None
 
 def do_deploy(archive_path):
     """Distributes an archive to the web servers"""
@@ -46,3 +59,10 @@ def do_deploy(archive_path):
         return True
     except:
         return False
+
+def deploy():
+    """Creates and distributes an archive to your web servers"""
+    archive_path = do_pack()
+    if not archive_path:
+        return False
+    return do_deploy(archive_path)
