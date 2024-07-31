b3be40f3d31cbbfca551b60d08c3b783e251cb19
saadelhamidi <saadavicii123@gmail.com>
2024-07-31 02:39:31 -0700

    x

--- web_flask/1-hbnb_route.py
+++ web_flask/1-hbnb_route.py
@@ -12,5 +12,11 @@ def hello_HBNB():
     return "Hello HBNB!"
 
 
+@app.route('/hbnb', strict_slashes=False)
+def Hbnb_page():
+    """ Hbnb page """
+    return "HBNB"
+
+
 if __name__ == "__main__":
     app.run(host='0.0.0.0', port=5000)
